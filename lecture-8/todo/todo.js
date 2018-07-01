4b5eab4caab341ab701f169b3c975f0080d62723
aayusharora <aayush113002@gmail.com>
2018-07-01 13:26:14 +0530

    Added lecture-8 code

--- lecture-8/todo/todo.js
+++ lecture-8/todo/todo.js
@@ -9,52 +9,51 @@ window.onload = function(){
     let input = document.getElementById('inp');
 
     btn.onclick = function(){
-       value = `<li>
-                    <input type="hidden">
-                    <span onclick="changeType(this)">${input.value}</span> 
-                    <button onclick="update(this)" id=${todoList.length + 1}>Update</button>  
-                </li>  
-      `;
-       // result.innerHTML = value;
-        todoList.push(value);
-        display(todoList);
+
+       let li = document.createElement('li');
+       let span = document.createElement('span');
+       let input = document.createElement('input');
+       let button = document.createElement('button');
+
+       li.appendChild(input);
+       li.appendChild(span);
+       li.appendChild(button);
+
+       span.onclick = changeType;
+       button.onclick = update;
+       button.id = todoList.length + 1;
+       input.type = "hidden";
+
+       let inpVal = document.createTextNode(inp.value);
+       span.appendChild(inpVal);
+
+       let btnVal = document.createTextNode("Update");
+       button.appendChild(btnVal);
+       result.appendChild(li);
+       todoList.push(span);
     }
 
-    function display(todolist) {
-        let value = "";
-        todolist.forEach(function(todo){
-           value += todo;
-        })
-
-        result.innerHTML = value;
-    }
 
     //1.  FUNCTION UPDATE
     //2. FUNCTION DELETE
     //3. Optimize this method using CreateNode
 };
 
-function update(element) {
+function update() {
+    let element = this;
     // let val = document.getElementById('getValue').val;
     let eleParent =  element.previousElementSibling
     let val = eleParent.previousElementSibling.value;
 
     if(val) {
         eleParent.textContent = val;
+        todoList[element.id - 1].textContent = val;
         eleParent.previousElementSibling.type="hidden";
     }
-
-    todoList[element.id - 1] = `<li>
-                    <input type="hidden">
-                    <span onclick="changeType(this)">${val}</span> 
-                    <button onclick="update(this)">Update</button>  
-                </li>  
-      `;
-
 }
 
-function changeType(element, id){
-    console.log(element.id);
+function changeType(){
+    let element = this;
     element.previousElementSibling.type="text"
     element.previousElementSibling.value="";
 }
